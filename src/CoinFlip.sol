c81e38ebe03fb771444ac08602f663d646e7d9ae
mahin-a11y <modasiyamahin14@gmail.com>
2026-04-30 23:28:08 +0530

    CoinFlip conract is added
    Added CoinFlip contract with functionality to flip a coin and track consecutive wins.

--- src/CoinFlip.sol
+++ src/CoinFlip.sol
@@ -1,1 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract CoinFlip {
+    uint256 public consecutiveWins;
+    uint256 lastHash;
+
+    /* this number(factor ) is on the default visibilty of the uint256 which is internally so 
+    hacker can know this easily  */
+
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+
+    constructor() {
+        consecutiveWins = 0;
+    }
+
+    /* the core vulnerbality is for the randomness the contract use the 
+        previous blockhash and already known number the contract divide both and then decide the 
+         number */
+         
+    function flip(bool _guess) public returns (bool) {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+
+        if (lastHash == blockValue) {
+            revert();
+        }
+
+        lastHash = blockValue;
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+
+        if (side == _guess) {
+            consecutiveWins++;
+            return true;
+        } else {
+            consecutiveWins = 0;
+            return false;
+        }
+    }
+}
